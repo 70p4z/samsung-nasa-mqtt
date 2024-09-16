119d15138424d19c2c22d90f405de6328be88022
oto <git@oto>
2024-09-16 15:17:38 +0200

    ensure to restart the nasa bridge when it exits

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #sleep 15
 cd $(dirname $(readlink -f $0))
-screen -dmS samsung_nasa bash -c 'bash run.sh'
+screen -dmS samsung_nasa bash -c 'while [ true ] ; do bash run.sh ; sleep 1 ; done'
