017bbe5a6ac52e65bf908c06bce87f697dba4a21
oto <git@oto>
2025-02-27 20:59:43 +0100

    make easier running the script (don't need the full serial device name)

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ cd $(dirname $(readlink -f $0))
 
 #serial=/dev/serial/by-id/usb-STMicroelectronics_STM32_STLink_066BFF554857707067031522-if02
 serial=/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A600JLJ7-if00-port0
-#serial=/dev/serial/by-id/usb-0483_5740-if00
-
+serial=/dev/serial/by-id/usb-0483_5740-if00
+serial=/dev/serial/by-id/`ls /dev/serial/by-id/ | head -n1`
 socat $serial,raw,echo=0,nonblock,min=0,b9600,parenb tcp-listen:7001,reuseaddr,fork &
 
 python3 samsung_mqtt_home_assistant.py &
