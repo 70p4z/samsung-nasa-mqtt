07aae50c27d34d4fd00b0600ac00503759624a1f
oto <git@oto>
2025-08-26 14:16:54 +0200

    in docker, don't use a screen process as it releases the main thread and kills the container

--- startup_docker.sh
+++ startup_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-#sleep 15
 cd $(dirname $(readlink -f $0))
-screen -dmS samsung_nasa bash -c 'while [ true ] ; do bash run.sh --mqtt-host host.docker.internal ; sleep 1 ; done'
+while [ true ]
+do 
+	bash run.sh --mqtt-host host.docker.internal 
+	sleep 1 
+done
