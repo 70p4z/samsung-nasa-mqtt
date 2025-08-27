98f5b2aefebf2b7199cf1e40da50e04fcba108f1
oto <git@oto>
2025-08-27 14:47:07 +0200

    avoid reporting error on the first run on haos

--- haos_ssh_22222_manual_setup.sh
+++ haos_ssh_22222_manual_setup.sh
@@ -4,6 +4,6 @@ BUILD_ARCH=`cat /etc/lfs-release | grep SUPERVISOR_ARCH |cut -f2 -d=`
 # build image
 docker build --build-arg BUILD_ARCH=$BUILD_ARCH --no-cache -t samsung_nasa_image .
 # delete and create privileged container with access to serial ports
-docker stop samsung_nasa
-docker container rm samsung_nasa
+docker stop samsung_nasa 2>&1
+docker container rm samsung_nasa 2>&1
 docker run -d --privileged -v /dev/serial/by-id/:/dev/serial/by-id/ -v /dev:/dev --add-host host.docker.internal:host-gateway --name samsung_nasa samsung_nasa_image
