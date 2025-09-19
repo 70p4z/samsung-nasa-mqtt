60e8cef8186edc95dd0ed4e941de2fc30221d6be
oto <git@oto>
2025-09-19 10:25:24 +0200

    add tmp mount on host to allow for easier log export

--- haos_ssh_22222_manual_setup.sh
+++ haos_ssh_22222_manual_setup.sh
@@ -28,4 +28,4 @@ docker build --build-arg BUILD_ARCH=$BUILD_ARCH --progress=plain --no-cache -t s
 # delete and create privileged container with access to serial ports
 docker stop samsung_nasa 2>&1
 docker container rm samsung_nasa 2>&1
-docker run -d --privileged -v /dev/serial/by-id/:/dev/serial/by-id/ -v /dev:/dev --add-host host.docker.internal:host-gateway --name samsung_nasa samsung_nasa_image
+docker run -d --privileged -v /dev/serial/by-id/:/dev/serial/by-id/ -v /dev:/dev -v /tmp:/tmp --add-host host.docker.internal:host-gateway --name samsung_nasa samsung_nasa_image
